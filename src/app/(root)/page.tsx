e1522d241681f6d356ad0659a5dc98a294726353
Abdelaziz <abdelazizmohamed1233@gmail.com>
2025-04-29 15:07:51 +0300

    add husky and lint-staged

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,6 +5,6 @@ import { useState } from "react";
 export default function Home() {
   console.log("helloo");
   const [email,setEmail]=useState<string>()
-  console.log(email)
+  // console.log(email)
   return <h1 className="text-3xl" onClick={()=>setEmail('hello')}>welcome to new project</h1>;
 }
